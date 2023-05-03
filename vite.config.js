2c650334803a23626c1c45421f6d5880e98f7223
Mariusz Trocewicz <throcu@gmail.com>
2023-05-03 23:15:38 +0200

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import handlebars from "vite-plugin-handlebars";
 const root = resolve(__dirname, "src");
 const outDir = resolve(__dirname, "dist");
 export default defineConfig({
+  base: "/vite-partials-template",
   plugins: [
     handlebars({
       partialDirectory: resolve(root, "partials"),
